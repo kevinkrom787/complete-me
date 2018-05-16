fe90f6fba034b64e88868a1e54384dc00a94c75c
Kevin Krom <kevinkrom787@gmail.com>
2018-05-16 16:23:30 -0600

    Add tests for insert

--- test/index-test.js
+++ test/index-test.js
@@ -1,8 +1,17 @@
 import { assert } from 'chai';
+import Tree from '../lib/Tree';
 
-describe('test', function () {
-  it('should return true', function () {
-    assert.equal(true, true);
-  });
+describe('index', function () {
+  let tree;
+
+
+  beforeEach(() => {
+    tree = new Tree()
+  })
+
+  // it('should exist', () => {
+  //   expect(tree.populate)
+  // })
+
 });
 
--- test/tree-test.js
+++ test/tree-test.js
@@ -6,23 +6,59 @@ describe('Tree', () => {
   let node;
   let tree;
 
+
 beforeEach(() => {
   tree = new Tree();
 
 })
 
-it('should return true', () => {
-  expect(true).to.equal(true);
+it('should exist', () => {
+  expect(tree).to.exist;
+})
+it('should have a root Node', () => {
+  expect(tree.root).to.exist;
+})
+it('should have a counter property', () => {
+  expect(tree.counter).to.equal(0);
 })
 
 describe('insert', () => {
   
   it('should be able to add a node to the tree', () => {
-    tree.insert('hi', )
-    expect(tree.root.children).to.equal('h');
+    tree.insert('hi')
+    expect(Object.keys(tree.root.children)).to.deep.equal(['h']);
+  })
+  it('should be able to add multiple nodes to tree', () => {
 
+    tree.insert('dog');
+    tree.insert('dogs');
+    tree.insert('hog');
+    tree.insert('doggo');
+    tree.insert('dogboh');
+
+    console.log(JSON.stringify(tree, null, 4))
+    expect(Object.keys(tree.root.children.d.children)).to.deep.equal(['o']);
   })
 
+  describe('suggest', () => {
+
+    it('should be able to suggest a word', () => {
+      tree.insert('dog');
+      tree.insert('dogs');
+      tree.insert('hog');
+      tree.insert('log')  
+      tree.insert('toggle')  
+      tree.suggest('d');
+      console.log(JSON.stringify(tree, null, 4));
+      expect('dog', 'dogs', 'hogs', 'log', 'toggle', ).to.equal('dog', 'dogs');
+    })
+
+    it('should return empty array if no words are inserted', () => {
+      expect(['']).to.deep.equal(['']);
+    })
+
+    it('should ')
+  })
 
 })
 
